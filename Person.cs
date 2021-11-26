f5c228453730ac9ca9a11240ae4be570e5bcdf41
Maria <Maria@LAPTOP-1LJ0SGIV>
2021-11-26 10:28:03 +0100

    Aggiungere i file di progetto.

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2.Maria.EsercitazioneFinale
+{
+    public class Person
+    {
+        public string FirstName { get; set; }    
+        public string SecondName { get; set; }
+        public string CodiceFiscale { get; set; }
+    }
+    public Person ()
+    {
+
+    }
+    //costruttore con parametri
+    public Person (string firstName string secondName string codicefiscale)
+    {
+        FirstName = firstName;
+        SecondName = secondName;
+        Codicefiscale = codicefiscale;
+    }
+
+    public override string ToString()
+    {
+        return $"[{CodiceFiscale}] - {FirstName} {SecondName}"
+    }
+ }
